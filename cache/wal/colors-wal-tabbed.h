fd82af10445a2f53d165ffcaeb8019d175ec39ce
bzix66 <permikinvacheslav@mail.ru>
2026-08-17 23:55:04 +0500

    Delete cache/wal directory

--- cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0d0d0d";
-static const char* selfgcolor   = "#d6d7da";
-static const char* normbgcolor  = "#8B8E95";
-static const char* normfgcolor  = "#d6d7da";
-static const char* urgbgcolor   = "#7D8188";
-static const char* urgfgcolor   = "#d6d7da";
